6894ac11a4f65594abbefea6f768158211a629da
Ujjwal Raj <123007468+ujjwalraj123@users.noreply.github.com>
2025-03-18 01:14:51 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://deploy-mern-app-1-api.vercel.app/auth/signup`;
+            const url = `https://auth-mern-app-delta.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
