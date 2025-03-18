3d910c993fa4924f4d532cf9a47bcded65cfbb0f
Ujjwal Raj <123007468+ujjwalraj123@users.noreply.github.com>
2025-03-18 22:07:16 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/signup`;
+            const url = `https://auth-mern-1nvpq2abk-ujjwal-rajs-projects-a7446681.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
